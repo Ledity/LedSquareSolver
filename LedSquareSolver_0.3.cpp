b30d0597dd0702bd417e86560b86c3cbdf33b9cc
Ledity <70322358+Ledity@users.noreply.github.com>
2020-08-29 23:33:18 +0300

    Tried to add unit test for 'int solveSquare ()'...
    ...but faced a bug. Please help me.

--- LedSquareSolver_0.3.cpp
+++ LedSquareSolver_0.3.cpp
@@ -2,8 +2,12 @@
 #include <math.h>
 #include <assert.h>
 
+//----------------------------------------------------
+
 const int ZERO_ROOTS = 0;
-const int SS_INF_ROOTS = -1;
+const int INF_ROOTS = -1;
+
+//----------------------------------------------------
 
 void input (double* a, double* b, double* c);
 
@@ -12,7 +16,13 @@ void clean_stdin ();
 int solveSquare (double a, double  b, double c,
 		double* x1, double* x2);
 
+//----------------------unittest----------------------
 
+void unittest_solveSquare ();
+
+
+
+//----------------------int main----------------------
 
 /**
  * \brief This function is responsible for IO
@@ -20,11 +30,12 @@ int solveSquare (double a, double  b, double c,
  * \vesion 0.2
  * \date Aug 2020
  *
- * This function outputs programme's name, purpose and asks the user to input a, b and c coefficents. Then it calles squareSolver. After that the function outputs roots.
+ * 	This function outputs programme's name, purpose and asks the user to input a, b and c coefficents. Then it calles squareSolver. After that the function outputs roots.
  */ 
 
 int main() 
 {
+	unittest_solveSquare ();
 	printf (" # LedSquareSolver\n"
 	        " # Программа для решения кв. уравнений\n"
 	        " # (c) Ledity, 2020\n\n"
@@ -32,7 +43,7 @@ int main()
 		" # Решение уравнения вида ax^2 + bx + c = 0\n"
 	        " # Введите a, b, c: ");
 
-	double a = -1.7E308, b = -1.7E308, c = -1.7E308;
+	double a, b, c;
 	input (&a, &b, &c);
 
 	double x1 = 0, x2 = 0;
@@ -49,7 +60,7 @@ int main()
 		case 2:
 			printf("\n x1 = %+lg,  x2 = %+lg\n", x1, x2);
 			break;
-		case SS_INF_ROOTS:
+		case INF_ROOTS:
 			printf("\n Корень - любое число.\n");
 			break;
 		default:
@@ -58,7 +69,7 @@ int main()
 	}
 }
 
-
+//----------------------------------------------------
 
 void input (double* a, double* b, double* c)
 {
@@ -84,7 +95,7 @@ void input (double* a, double* b, double* c)
 	}
 }
 
-
+//----------------------------------------------------
 
 void clean_stdin ()
 {
@@ -95,7 +106,7 @@ void clean_stdin ()
 	}
 }
 
-
+//----------------------------------------------------
 
 /**
  * \brief This function solves square equations
@@ -104,7 +115,7 @@ void clean_stdin ()
  * \date Aug 2020
  *
  *	This function recieves a, b and c coefficents of square equation and links to x1 and x2 variables of another function (such as int main), that will be transformed into sqruare roots.
- *	The function itself outputs number of roots, where SS_INFROOTS means infinite number of roots.
+ *	The function itself outputs number of roots, where INFROOTS means infinite number of roots.
  *	Use &x1 and &x2 in input to let int solveSquare сhange your x1 and x2 variables.
  */
 
@@ -123,7 +134,7 @@ int solveSquare (double a, double  b, double c,
 	{
 		if (c == 0) 
 		{
-			return SS_INF_ROOTS;
+			return INF_ROOTS;
 		}
 		else
 		{
@@ -150,4 +161,120 @@ int solveSquare (double a, double  b, double c,
 			return ZERO_ROOTS;
 		}
 	}
+}
+
+//----------------------unittest----------------------
+
+void unittest_solveSquare ()
+{
+	printf (" # Testing 'int solveSquare ()'.\n");
+	int nPassedTests = 0, nTests = 0;
+
+	int test_a, test_b, test_c;
+
+	double test_x1, test_x2;
+	int    test_func_out;
+
+	// Test #1. a = 1, b = -2, c = 1.
+	
+	nTests++;
+
+	test_a = 1;
+	test_b = -2;
+	test_c = 1;
+
+	if (test_func_out = solveSquare (test_a, test_b, test_c, &test_x1, &test_x2) == 1
+	    && test_x1 ==1)
+	{
+		nPassedTests++;
+	}
+	else
+	{
+		printf ("\n # UNITTEST ERROR: 'int solveSquare ()' test #%d failed.\n"
+			  "	input: a = %lg, b = %lg, c = %lg.\n"
+			  "	output: x1 = %lg, function out = %lg.\n",
+			nTests, test_a, test_b, test_c, test_x1, test_func_out);
+	};
+
+	// Test #2. a = 1, b = 3, c = 2
+	
+	nTests++;
+
+	test_a = 1;
+	test_b = 3;
+	test_c = 2;
+
+	if (test_func_out = solveSquare (test_a, test_b, test_c, &test_x1, &test_x2) == 2
+	    && (test_x1 == -1 && test_x2 == -2 || test_x1 == -2 && test_x1 == -1))
+	{
+		nPassedTests++;
+	}
+	else
+	{
+		printf ("\n # UNITTEST ERROR: 'int solveSquare ()' test #%d failed.\n"
+			  "	input: a = %lg, b = %lg, c = %lg.\n"
+			  "	output: x1 = %lg, x2 = %lg, function out = %lg.\n",
+			nTests, test_a, test_b, test_c, test_x1, test_x2, test_func_out);
+	};
+	
+	// Test #3. a = 0, b = 0, c = 0
+	
+	nTests++;
+
+	test_a = 0;
+	test_b = 0;
+	test_c = 0;
+
+	if (test_func_out = solveSquare (test_a, test_b, test_c, &test_x1, &test_x2) == ZERO_ROOTS)
+	{
+		nPassedTests++;
+	}
+	else
+	{
+		printf ("\n # UNITTEST ERROR: 'int solveSquare ()' test #%d failed.\n"
+			  "	input: a = %lg, b = %lg, c = %lg.\n"
+			  "	output: function out = %lg.\n",
+			nTests, test_a, test_b, test_c, test_func_out);
+	};
+
+	// Test #4. a = 0, b = 0, c = 2
+	
+	nTests++;
+
+	test_a = 0;
+	test_b = 0;
+	test_c = 2;
+
+	if (test_func_out = solveSquare (test_a, test_b, test_c, &test_x1, &test_x2) == INF_ROOTS)
+	{
+		nPassedTests++;
+	}
+	else
+	{
+		printf ("\n # UNITTEST ERROR: 'int solveSquare ()' test #%d failed.\n"
+			  "	input: a = %lg, b = %lg, c = %lg\n"
+			  "	output: x1 = %lg, x2 = %lg, function out = %lg.\n",
+			nTests, test_a, test_b, test_c, test_func_out);
+	};
+
+	// Test #5. a = 1, b = 2, c = 3
+	
+	nTests++;
+
+	test_a = 1;
+	test_b = 2;
+	test_c = 3;
+
+	if (test_func_out = solveSquare (test_a, test_b, test_c, &test_x1, &test_x2) == ZERO_ROOTS)
+	{
+		nPassedTests++;
+	}
+	else
+	{
+		printf ("\n # UNITTEST ERROR: 'int solveSquare ()' test #%d failed.\n"
+			  "	input: a = %lg, b = %lg, c = %lg\n"
+			  "	output: function out = %lg\n",
+			nTests, test_a, test_b, test_c, test_func_out);
+	};
+	printf("\n # Testing 'int solveSquare ()' complete. %d successuful, %d failed.\n\n", nPassedTests, nTests-nPassedTests);
 }
